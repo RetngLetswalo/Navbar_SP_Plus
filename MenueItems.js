9c1f29d12a7402683df98a38a9761b84ee756df9
Retang Letswalo <108868133+RetngLetswalo@users.noreply.github.com>
2022-08-11 08:33:21 +0200

    Create MenueItems.js

---
+++ MenueItems.js
@@ -0,0 +1,32 @@
+export const MenueItems = [
+    {
+        title: 'Overview',
+        url: '#',
+        cName:'nav-links',
+    },
+    {
+        title: 'Dashboard',
+        url: '#',
+        cName:'nav-links',
+    },
+    {
+        title: 'Generators',
+        url: '#',
+        cName:'nav-links',
+    },
+    {
+        title: 'Machine Learning',
+        url: '#',
+        cName:'nav-links',
+    },
+    {
+        title: 'Predictive Analytics',
+        url: '#',
+        cName:'nav-links',
+    },
+    {
+        title: 'Reporting',
+        url: '#',
+        cName:'nav-links',
+    },
+]
